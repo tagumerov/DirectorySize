4039a6097b6eb51f262ce02e5b151296782de8be
Tim <gtamail@mail.ru>
2019-08-26 03:42:50 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String directoryName;
+        File dir;
+        long size = 0;
+        System.out.println("Подсчет размера каталога или файла");
+        for (; ; ) {
+            System.out.println("Укажите путь к каталогу или файлу");
+            directoryName = reader.readLine();
+            dir = new File(directoryName);
+            if (!dir.isDirectory() && !dir.isFile()) {
+                System.out.println("Задан неправильный путь. Правильно, например, C:\\Program Files");
+                continue;
+            }
+            size = getDirSize(dir);
+            printSize(size);
+        }
+    }
+
+    public static long getDirSize(File dir) {
+        long size = 0;
+        if (dir.isFile()) {
+            size = dir.length();
+        } else {
+            File[] subFiles = dir.listFiles();
+            for (File file : subFiles) {
+                if (file.isFile()) {
+                    size += file.length();
+                } else {
+                    size += getDirSize(file);
+                }
+            }
+        }
+        return size;
+    }
+
+    public static void printSize(Long size)
+    {
+        long biteInKbite = 1024;
+        long biteInMbite = 1048576;
+        long biteInGbite = 1073741824;
+
+        if(size < biteInKbite) {
+            System.out.println("Размер: " + size + "байт");
+        }
+        else if(size < biteInMbite){
+            System.out.printf("Размер: %.3f килобайт", (double)size/biteInKbite);
+            System.out.println("");
+        }
+        else if(size < biteInGbite){
+            System.out.printf("Размер: %.3f мегабайт", (double)size/biteInMbite);
+            System.out.println("");
+        }else{
+            System.out.printf("Размер: %.3f гигабайт", (double)size/biteInGbite);
+            System.out.println("");
+        }
+    }
+}
